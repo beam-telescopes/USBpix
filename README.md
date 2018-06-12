345ba2d269db079196ff0dfafcc6dddabf48ab6a
Jan Dreyling-Eschweiler <jan.dreyling-eschweiler@desy.de>
2018-06-12 10:14:10 +0200

    README and environment script for Ubuntu16

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 check online documentation on
 http://icwiki.physik.uni-bonn.de/twiki/bin/view/Systems/UsbPix
+
+# Quick installation for Ubuntu 16
+
+under construction
---
+++ set_environments_ubuntu16.sh
@@ -0,0 +1,10 @@
+# for building STControl
+
+export EUDAQ=/opt/eudaq2
+export QT5DIR=/usr/lib/x86_64-linux-gnu/qt5
+export QWTDIR=/usr/lib/x86_64-linux-gnu
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/eudaq2/lib
+
+echo "Go to STcontrol folder and run setup.sh"
+echo "e.g. source setup.sh -gpib no"
