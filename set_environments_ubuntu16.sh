077537d7263971813abf780c5e327205609dd148
fhlteledev <fhlteledev@desy.de>
2018-09-10 14:43:10 +0200

    update env script

--- set_environments_ubuntu16.sh
+++ set_environments_ubuntu16.sh
@@ -1,10 +1,16 @@
 # for building STControl
-
 export EUDAQ=/opt/eudaq2
 export QT5DIR=/usr/lib/x86_64-linux-gnu/qt5
 export QWTDIR=/usr/lib/x86_64-linux-gnu
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/eudaq2/lib
+# setup USBpix installation
+echo "cd USBpix && source setup.sh -gpib no && make -j4"
 
-echo "Go to STcontrol folder and run setup.sh"
-echo "e.g. source setup.sh -gpib no"
+# for re-compiling
+# make clean
+# make distclean
+
+# for running
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$EUDAQ/lib
+# or copy eudaq lib 
+echo "./USBpix/bin/STcontrol_eudaq"
